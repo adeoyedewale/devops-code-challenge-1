cff211f123bc52a2ffc66b0bf91815ea3f843468
Adewale Oyedele <88012353+adeoyedewale@users.noreply.github.com>
2023-05-01 13:51:13 -0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = 'http://localhost:8080/'
-export const API_URL = 'http://example-lb-844904395.us-east-1.elb.amazonaws.com:80/'
+export const API_URL = 'http://example-lb-7125103.us-east-1.elb.amazonaws.com/'
 export default API_URL
